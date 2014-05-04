de924190f0011c2056bbbfde91b73bfad337c21d
rssilva <rafaelspdasilva@gmail.com>
2014-05-04 02:39:49 -0300

    adding login stuff for demo

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="index.php" method="POST">
+		<input name="pass" type="password">
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
